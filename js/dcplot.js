d227dfabfed60a5a6756a797245876ca681f4d88
Chris Berthiaume <chrisbee@uw.edu>
2015-03-11 17:07:59 -0700

    Implemented dc.js SeriesChart for population-wise plots. In theory it's possible to filtered out populations now. Get total concentration from database, properly handling nulls. Need to fix bug in the way nulls/missing time points are handled in plots.

--- js/dcplot.js
+++ js/dcplot.js
@@ -4,6 +4,7 @@ var charts = {};
 var popNames = ["prochloro", "synecho", "picoeuk", "beads"];
 // Full names for legend
 var popLabels = ["Prochlorococcus", "Synechococcus", "Picoeukaryotes", "Beads"];
+var popLookup = {"prochloro": "Prochlorococcus", "synecho": "Synechococcus", "picoeuk": "Picoeukaryotes", "beads": "Beads"};
 
 function executeSqlQuery(query, cb) {
     var sqlshare_query_url = 'https://rest.sqlshare.escience.washington.edu/REST.svc/execute?sql=';
@@ -29,42 +30,71 @@ function logSqlQuery(query) {
     });
 }
 
-// Turn jsonp data from SQL share query result into an array of JSON objects
-// keyed by column header, with time converted to Date().getTime()
+// Turn jsonp data from SQL share query result into an arrays of JSON objects
+// that can be easily fed to crossfilter/dc.js
 function transformData(jsonp) {
     if (jsonp.header.length < 2) {
         alert('Query ' + data.sql + ' returned ' + jsonp.header.length + ' columns, needs at least 2');
         return;
     }
-    var values = [];
+
+    // Figure out which columns correspond to which column headers
+    idx = {};
+    for (var col = 0; col < jsonp.header.length; col++) {
+        idx[jsonp.header[col]] = col;
+    }
+
+    var sflValues = [];  // environmental data
+    var popValues = [];  // population data
+
     var msecMinute = 60 * 1000;
     var prevTime = null;
     for (var i in jsonp.data) {
-        var j = {}
-        j.time = new Date(jsonp.data[i][0]);
+        var curTime = new Date(jsonp.data[i][idx["time"]]);
 
         // If this record is more than 4 minutes from last record, assume
         // records are missing and add a placeholder empty record. Only need 
         // one placeholder record per gap.  This placeholder record makes it
         // possible to draw line chart with gaps.
-        if (i > 0 && (j.time - prevTime) > 4 * msecMinute) {
-            var placeholder = { time: new Date(prevTime.getTime() + (3 * msecMinute)) };
-            for (var col = 0; col < jsonp.header.length - 1; ++col) {
-                placeholder[jsonp.header[col+1]] = null;
-            }
-            placeholder["total_conc"] = null;
-            values.push(placeholder);
-        }
+        /*if (i > 0 && (curTime - prevTime) > 4 * msecMinute) {
+            sflValues.push({
+                time: new Date(prevTime.getTime() + (3 * msecMinute)),
+                salinity: null,
+                ocean_tmp: null,
+                par: null,
+                total_conc: null
+            });
+            popNames.forEach(function(pn) {
+                popValues.push({
+                    time: new Date(prevTime.getTime() + (3 * msecMinute)),
+                    size: null,
+                    conc: null,
+                    pop: pn
+                });
+            });
+        }*/
 
-        for (var col = 0; col < jsonp.header.length - 1; ++col) {
-            j[jsonp.header[col+1]] = jsonp.data[i][col+1];
-        }
-        j["total_conc"] = 0;
-        popNames.forEach(function(n) { j["total_conc"] += j[n + "_conc"]; });
-        values.push(j);
-        prevTime = j.time;
+        sflValues.push({
+            time: curTime,
+            ocean_tmp: jsonp.data[i][idx["ocean_tmp"]],
+            salinity: jsonp.data[i][idx["salinity"]],
+            par: jsonp.data[i][idx["par"]],
+            total_conc: jsonp.data[i][idx["total_conc"]]
+        });
+
+        popNames.forEach(function(pop) {
+            popValues.push({
+                time: curTime,
+                conc: jsonp.data[i][idx[pop + "_conc"]],
+                size: jsonp.data[i][idx[pop + "_size"]],
+                pop: pop
+            });
+        });
+
+        prevTime = curTime;
     }
-    return values;
+
+    return { sfl: sflValues, pop: popValues };
 }
 
 // Reduce functions to stop crossfilter from coercing null values to 0
@@ -98,9 +128,19 @@ function reduceInitial() {
 // traversing entire data set every time.
 function recalculateY(chart) {
     var filter = chart.filter();
+    if (chart.children !== undefined) {
+        // Population series plot
+        // key for dimension is [time, pop]
+        var key = function(element) { return element.key[0]; };
+    } else {
+        // Single line chart
+        // key for dimension is time
+        var key = function(element) { return element.key; };
+    }
+
     var valuesInRange = chart.group().all().filter(function(element, index, array) {
-            return (element.key >= filter[0] && element.key < filter[1]);
-        });
+        return (key(element) >= filter[0] && key(element) < filter[1]);
+    });
     var minMaxY = d3.extent(valuesInRange, function(d) { return d.value; });
     // Add 10% headroom above and below
     var diff = minMaxY[1] - minMaxY[0];
@@ -109,7 +149,40 @@ function recalculateY(chart) {
     chart.y(d3.scale.linear().domain(minMaxY));
 }
 
-function plotTimeSeries(ndx, timeDim, key, yAxisLabel) {
+function filteredHandler(chart, filter) {
+    // Only draw dots if size of filter range is <= 12 hours
+    var maxDotRange = 1000 * 60 * 60 * 12 * 1;
+    var dotOptions = {
+        radius: 2,
+        fillOpacity: 0.8,
+        strokeOpacity: 0.8
+    };
+    if ((filter[1] - filter[0]) <= maxDotRange) {
+        if (chart.children !== undefined) {
+            chart.children().forEach(function(c) {
+                c.renderDataPoints(dotOptions);
+            });
+        } else {
+            chart.renderDataPoints(dotOptions);
+        }
+    } else {
+        if (chart.children !== undefined) {
+            chart.children().forEach(function(c) {
+                c.renderDataPoints(false);
+            });
+        } else {
+            chart.renderDataPoints(false);
+        }
+    }
+
+    // Recalculate Y domain, but don't redraw or render
+    recalculateY(chart);
+
+    // Have to render, not redraw, to fix renderDataPoint changes
+    chart.render();
+}
+
+function plotLineChart(timeDim, key, yAxisLabel) {
     var chart = dc.lineChart("#" + key);
     charts[key] = chart;
 
@@ -128,82 +201,43 @@ function plotTimeSeries(ndx, timeDim, key, yAxisLabel) {
         .brushOn(false)
         .clipPadding(10)
         .yAxisLabel(yAxisLabel)
-        .renderHorizontalGridLines(true)
         .dimension(timeDim)
         .group(keyByTimeGroup)
         .defined(function(d) { return (d.y !== null); })  // don't plot segements with missing data
         .title(function(d) {
             return d.key + '\n' + d3.format(".3n")(d.value);
         })
-        .on("filtered", function(chart, filter) {
-            // Only draw dots if size of filter range is <= 12 hours
-            var maxDotRange = 1000 * 60 * 60 * 12 * 1;  
-            if ((filter[1] - filter[0]) <= maxDotRange) {
-                chart.renderDataPoints({
-                    radius: 2,
-                    fillOpacity: 0.8,
-                    strokeOpacity: 0.8
-                });
-                console.log(chart.yAxisLabel(), chart.yAxisMin(), chart.yAxisMax());
-            } else {
-                chart.renderDataPoints(false);
-            }
-
-            // Recalculate Y domain
-            recalculateY(chart);
-
-            // Have to render, not redraw, to fix all points in plot
-            chart.render();
-            //chart.redraw();
-        });
+        .on("filtered", filteredHandler);
     chart.render();
 }
 
-function plotPopulations(ndx, timeDim, key, yAxisLabel) {
-    var chart = dc.compositeChart("#" + key);
+function plotSeriesChart(timeDim, timePopDim, key, yAxisLabel) {
+    var chart = dc.seriesChart("#" + key);
     charts[key] = chart;
 
-    var numberFormat = d3.format(".3n");
+    /*keyGroup = timePopDim.group().reduce(
+        reduceAdd(key), reduceRemove(key), reduceInitial);*/
+    keyGroup = timePopDim.group().reduceSum(function(d) { return d[key]; });
 
     var minMaxTime = [timeDim.bottom(1)[0].time, timeDim.top(1)[0].time];
-
-    var groups = {};
-    var minMaxY = [0, 0];
-    var compose = [];
-    var colors = ["red", "green", "orange", "gray"];
-    for (var i in popNames) {
-        var popKey = popNames[i] + "_" + key;
-        var group = timeDim.group().reduce(
-            reduceAdd(popKey), reduceRemove(popKey), reduceInitial);
-        var minMax = d3.extent(group.all(), function(d) { return d.value; });
-        if (minMax[0] <= minMaxY[0]) {
-            minMaxY = minMax[0];
-        }
-        if (minMax[1] > minMaxY[1]) {
-            minMaxY[1] = minMax[1];
-        }
-        var childChart = dc.lineChart(chart)
-            .dimension(timeDim)
-            .group(group, popLabels[i])
-            .colors(colors[i])
-        compose.push(childChart);
-        charts[popKey] = childChart;
-    }
-
+    var minMaxY = d3.extent(keyGroup.all(), function(p) { return p.value; });
     chart
         .width(768)
         .height(150)
+        .chart(dc.lineChart)
         .x(d3.time.scale.utc().domain(minMaxTime))
         .y(d3.scale.linear().domain(minMaxY))
+        .dimension(timePopDim)
+        .group(keyGroup)
+        .seriesAccessor(function(d) { return popLookup[d.key[1]]; })
+        .keyAccessor(function(d) { return d.key[0]; })
+        .valueAccessor(function(p) { return p.value; })
         .brushOn(false)
         .clipPadding(10)
         .yAxisLabel(yAxisLabel)
-        .renderHorizontalGridLines(true)
-        .dimension(timeDim)
         .title(function(d) {
-            return d.key + '\n' + d3.format(".3n")(d.value);
+            return d.key[0] + '\n' + d.key[1] + "\n" + d3.format(".3n")(d.value);
         })
-        .compose(compose)
         .legend(dc.legend()
             .x(75)
             .y(100)
@@ -218,56 +252,28 @@ function plotPopulations(ndx, timeDim, key, yAxisLabel) {
                 return (d.y !== null);
             }
         })
-        .on("filtered", function(chart, filter) {
-            // Only draw dots if size of filter range is <= 12 hours
-            var maxDotRange = 1000 * 60 * 60 * 12 * 1;
-            if ((filter[1] - filter[0]) <= maxDotRange) {
-                chart.children().forEach(function(c) {
-                    c.renderDataPoints({
-                        radius: 2,
-                        fillOpacity: 0.8,
-                        strokeOpacity: 0.8
-                    });
-                });
-                popNames.forEach(function(n) {
-                    var popKey = n + "_" + key;
-                    var c = charts[popKey];
-                    console.log(popKey, c.yAxisMin(), c.yAxisMax());
-                });
-            } else {
-                chart.children().forEach(function(c) {
-                    c.renderDataPoints(false);
-                });
-            }
-
-            // Recalculate Y domain
-            //recalculateY(chart);
-
-            // Have to render, not redraw, to fix all points in plot
-            chart.render();
-            //chart.redraw();
-        });
+        .on("filtered", filteredHandler);
     chart.margins().bottom = 20
     chart.render();
 }
 
-function plotRangeChart(ndx, timeDim) {
+function plotRangeChart(timeDim, key, yAxisLabel) {
     var rangeChart = dc.lineChart("#rangeChart");
     charts["rangeChart"] = rangeChart;
 
     var minMaxTime = [timeDim.bottom(1)[0].time, timeDim.top(1)[0].time];
-    var totalConcByTime = timeDim.group().reduce(
-            reduceAdd("total_conc"), reduceRemove("total_conc"), reduceInitial);
-    var minMaxY = d3.extent(totalConcByTime.all(), function(d) { return d.value; });
+    var keyByTime = timeDim.group().reduce(
+            reduceAdd(key), reduceRemove(key), reduceInitial);
+    var minMaxY = d3.extent(keyByTime.all(), function(d) { return d.value; });
     rangeChart
         .width(768)
         .height(100)
         .x(d3.time.scale.utc().domain(minMaxTime))
         .y(d3.scale.linear().domain(minMaxY))
         .clipPadding(10)
-        .yAxisLabel("Total Concentration")
+        .yAxisLabel(yAxisLabel)
         .dimension(timeDim)
-        .group(totalConcByTime)
+        .group(keyByTime)
         .defined(function(d) { return (d.y !== null); });  // don't plot segements with missing data
     rangeChart.render();
     rangeChart.on("filtered", function(chart, filter) {
@@ -280,23 +286,39 @@ function plotRangeChart(ndx, timeDim) {
 }
 
 function plot(jsonp) {
-    var data = transformData(jsonp);  // array of JSON objects
-    var ndx = crossfilter(data);
-    var timeDim = ndx.dimension(function(d) { return d.time; });
+    var data = transformData(jsonp);
 
-    plotTimeSeries(ndx, timeDim, "ocean_tmp", "Temperature (C)");
-    plotTimeSeries(ndx, timeDim, "salinity", "Salinity");
-    plotTimeSeries(ndx, timeDim, "par", "PAR");
-    plotPopulations(ndx, timeDim, "conc", "Concentration");
-    plotPopulations(ndx, timeDim, "size", "Size (forward-scatter)");
-    plotRangeChart(ndx, timeDim);
+    var sflxf = crossfilter(data["sfl"]);
+    var popxf = crossfilter(data["pop"]);
+
+    var timeDim = sflxf.dimension(function(d) { return d.time; });
+    var timePopDim = popxf.dimension(function(d) { return [d.time, d.pop]; });
+    var popDim = popxf.dimension(function(d) { return d.pop; });
+
+    plotLineChart(timeDim, "ocean_tmp", "Temp (degC)");
+    plotLineChart(timeDim, "salinity", "Salinity (psu)");
+    plotLineChart(timeDim, "par", "PAR (w/m2)");
+
+    plotSeriesChart(timeDim, timePopDim, "conc", "Abundance (10^6 cells/L)");
+    plotSeriesChart(timeDim, timePopDim, "size", "Forward scatter (a.u.)");
+
+    plotRangeChart(timeDim, "total_conc", "Total Abundance (10^6 cells/L)");
+
+    /*setTimeout(function() {
+        console.log(timePopDim.groupAll().reduceCount().value());
+        popDim.filter("prochloro");
+        console.log(timePopDim.groupAll().reduceCount().value());
+        dc.redrawAll();
+        console.log(charts["par"].filter());
+        console.log(charts["conc"].filter());
+    }, 5000);*/
 }
 
 function initialize() {
-    var query = 'SELECT * ';
+    var query = 'SELECT *, (IsNull(prochloro_conc, 0) + IsNull(synecho_conc, 0) + IsNull(picoeuk_conc, 0) + IsNull(beads_conc, 0)) as total_conc ';
     query += 'FROM [seaflow.viz@gmail.com].[seaflow all query] ';
     query += 'ORDER BY [time] ASC';
     executeSqlQuery(query, plot);
 }
 
-initialize()
+initialize();
